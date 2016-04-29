db4777c3965592c6f3c9939c87d137e112477a89
ceberous <cerbus.collin@gmail.com>
2016-04-28 21:25:20 -0700

    why am I making this harder than need be

--- grabWeather.py
+++ grabWeather.py
@@ -11,31 +11,36 @@ def getNextHighTide():
 
 	cT = getCurrentTime()
 	cT = cT.split(":")
-	cTHours 	= cT[0]
+	cTHours 	= int( cT[0] )
 	cTMinutes 	= cT[1]
 
 	h1 	= high1Time.split(":")
 	h1Hours  	= h1[0]
 	h1Minutes 	= h1[1]
 	h1AMPM		= h1Minutes[-2] + h1Minutes[-1]
-	h1Minutes   = h1Minutes[:-2]
+	h1Minutes   = int( h1Minutes[:-2] )
 
 	h2 = high2Time.split(":")
 	h2Hours		= h2[0]
 	h2Minutes 	= h2[1]
 	h2AMPM 		= h2Minutes[-2] + h2Minutes[-1]
-	h2Minutes	= h2Minutes[:-2]
+	h2Minutes	= int( h2Minutes[:-2] )
 
 	#24 Hour Conversion
-	h1Hours = ( (str( int(h1Hours) + 12 )) , (h1Hours) )[h1AMPM == "am"]
-	h2Hours = ( (str( int(h2Hours) + 12 )) , (h1Hours) )[h2AMPM == "am"]
+	h1Hours = ( ( int(h1Hours) + 12 ) , (h1Hours) )[h1AMPM == "am"]
+	h2Hours = ( ( int(h2Hours) + 12 ) , (h2Hours) )[h2AMPM == "am"]
 
-	print( "Current Time = " + cTHours + ":"  + cTMinutes )
-	print( "H1-Time = " + h1Hours + ":"  + h1Minutes + " " + h1AMPM )
-	print( "H2-Time = " + h2Hours + ":"  + h2Minutes + " " + h2AMPM )
+	#print( "Current Time = " + cTHours + ":"  + cTMinutes )
+	#print( "H1-Time = " + h1Hours + ":"  + h1Minutes + " " + h1AMPM )
+	#print( "H2-Time = " + h2Hours + ":"  + h2Minutes + " " + h2AMPM )
 
 
-
+	#if ( cTHours > h1Hours ) :
+	hoursLeft = h2Hours - cTHours
+	x =  h2Minutes + ( 60 - h2Minutes )
+	hoursLeft = str(hoursLeft)
+	x = str(x)
+	print("next high tide = " + hoursLeft + ":" + x )
 
 
 d = PyQuery( url='http://tides.willyweather.com/wa/mason-county/lilliwaup.html' )
